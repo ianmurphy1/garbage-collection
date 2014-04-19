3701ea4633d7418d82c13f15606269a69c3378ff
Ian Murphy <iano200@gmail.com>
2014-04-19 10:21:50 +0100

    Initial Commit

---
+++ src/tree/Node.java
@@ -0,0 +1,8 @@
+package tree;
+
+/**
+ * @author Ian Murphy - 20057028
+ *         Date: 19/04/2014
+ */
+public class Node {
+}
---
+++ src/ui/Main.java
@@ -0,0 +1,8 @@
+package ui;
+
+/**
+ * @author Ian Murphy - 20057028
+ *         Date: 19/04/2014
+ */
+public class Main {
+}
