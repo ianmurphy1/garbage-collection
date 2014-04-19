5d015bde8815447e66371c3690e4a49246a64354
Ian Murphy <iano200@gmail.com>
2014-04-19 18:35:07 +0100

    Completed tree structure

--- src/tree/Tree.java
+++ src/tree/Tree.java
@@ -1,5 +1,10 @@
 package tree;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author Ian Murphy - 20057028
  *         Date: 19/04/2014
@@ -65,5 +70,80 @@ public class Tree<T> {
 
     public boolean isEmpty() { return root == null; }
 
+    public List<Node<T>> build(Order traversalOrder) {
+        List<Node<T>> result = null;
+        if (root != null)
+            result = build(root, traversalOrder);
+
+        return result;
+    }
+
+
+    public List<Node<T>> build(Node<T> node, Order traversalOrder) {
+        List<Node<T>> result = new ArrayList<Node<T>>();
+
+        if (traversalOrder == Order.PRE_ORDER)
+            buildPreOrder(node, result);
+        else if (traversalOrder == Order.POST_ORDER)
+            buildPostOrder(node, result);
+
+        return result;
+    }
+
+    private void buildPreOrder(Node<T> node, List<Node<T>> traversalResult) {
+        traversalResult.add(node);
+        for (Node<T> child : node.getChildren())
+            buildPreOrder(child, traversalResult);
+    }
+
+    private void buildPostOrder(Node<T> node, List<Node<T>> traversalResult) {
+        for (Node<T> child : node.getChildren())
+            buildPostOrder(child, traversalResult);
+
+        traversalResult.add(node);
+    }
+
+    public Map<Node<T>, Integer> buildWithDepth(Order traversalOrder) {
+        Map<Node<T>, Integer> result = null;
+        if (root != null)
+            result = buildWithDepth(root, traversalOrder);
+        return result;
+    }
+
+    public Map<Node<T>, Integer> buildWithDepth(Node<T> node, Order traversalOrder) {
+        Map<Node<T>, Integer> traversalResult = new LinkedHashMap<Node<T>, Integer>();
+        if (traversalOrder == Order.PRE_ORDER)
+            buildPreOrderWithDepth(node, traversalResult, 0);
+        else if (traversalOrder == Order.POST_ORDER)
+            buildPostOrderWithDepth(node, traversalResult, 0);
+
+        return traversalResult;
+    }
+
+    private void buildPreOrderWithDepth(Node<T> node, Map<Node<T>, Integer> traversalResult, int depth) {
+        traversalResult.put(node, depth);
+        for (Node<T> child : node.getChildren())
+            buildPreOrderWithDepth(child, traversalResult, depth + 1);
+    }
+
+    private void buildPostOrderWithDepth(Node<T> node, Map<Node<T>, Integer> traversalResult, int depth) {
+        for (Node<T> child : node.getChildren())
+            buildPostOrderWithDepth(child, traversalResult, depth + 1);
+        traversalResult.put(node, depth);
+    }
+
+    public String toString() {
+        String str = "";
+        if (root != null)
+            str = build(Order.PRE_ORDER).toString();
+        return str;
+    }
+
+    public String toStringWithDepth() {
+        String str = "";
+        if (root != null)
+            str = buildWithDepth(Order.PRE_ORDER).toString();
+        return str;
+    }
 
 }
