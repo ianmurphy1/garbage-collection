c4958d73c3abd5c73e6f6882fb5b9b0bcc632271
Ian Murphy <iano200@gmail.com>
2014-04-20 14:06:51 +0100

    Done for now, back at it at later date

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,18 +1,15 @@
 package ui;
 
 import gc.GarbageCollection;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
 /**
  * @author Ian Murphy - 20057028
  *         Date: 19/04/2014
  */
-public class Main {
-    GarbageCollection gc;
-
-    public Main() {
-        gc = new GarbageCollection();
+public class Main extends Application{
+    @Override
+    public void start(Stage stage) throws Exception {
     }
-
-
-
 }
