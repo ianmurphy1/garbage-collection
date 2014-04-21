49c5c79e194c2585bcce75fbf04de40aad46334e
Ian Murphy <iano200@gmail.com>
2014-04-21 13:05:38 +0100

    Started GUI. 650WAT\!

--- src/ui/MainController.java
+++ src/ui/MainController.java
@@ -4,5 +4,5 @@ package ui;
  * @author Ian Murphy - 20057028
  *         Date: 19/04/2014
  */
-public class Controller {
+public class MainController {
 }
---
+++ src/ui/MemoryController.java
@@ -0,0 +1,8 @@
+package ui;
+
+/**
+ * @author Ian Murphy - 20057028
+ *         Date: 21/04/2014
+ */
+public class MemoryController {
+}
