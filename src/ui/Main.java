c95f43bffc059de63f30f7d748aa08de7a22509a
Ian Murphy <iano200@gmail.com>
2014-04-24 18:55:14 +0100

    Main comments done

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -13,6 +13,8 @@ import ui.controllers.MemoryController;
 import ui.controllers.RefController;
 
 /**
+ * Class which runs the app.
+ *
  * @author Ian Murphy - 20057028
  *         Date: 19/04/2014
  */
@@ -24,34 +26,71 @@ public class Main extends Application{
     GCController gcCon;
     Scene scene;
 
+    /**
+     * Method that returns this objects scene
+     * @return App's Scene
+     */
     public Scene getScene() {
         return scene;
     }
 
+    /**
+     * Constructor for the application which creates a new
+     * instance of the garbage collector
+     */
     public Main() {
         this.gc = new GarbageCollection();
     }
 
+    /**
+     * Method that returns the app's MainController
+     * @return App's MainController
+     */
     public MainController getMc() {
         return mc;
     }
 
+    /**
+     * Method that returns the app's MemoryController
+     * @return App's MemoryController
+     */
     public MemoryController getMemc() {
         return memc;
     }
 
+    /**
+     * Method that returns the app's reference controller
+     * @return App's ReferenceController
+     */
     public RefController getRefc() {
         return refc;
     }
 
+    /**
+     * Method that returns the app's garbage collection
+     * controller
+     * @return App's GarbageCollectionController
+     */
     public GCController getGcCon() {
         return gcCon;
     }
 
+    /**
+     * Main Method of the application that starts the program
+     * @param args
+     */
     public static void main(String[] args) {
         Application.launch(args);
     }
 
+    /**
+     * Method that gets called when the application starts.
+     * Gets the apps controllers and sets their app to be this instance of
+     * Main. Sets the size of the window.
+     *
+     * @param primaryStage
+     * @throws Exception
+     */
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader mainFxmlLoader = new FXMLLoader(Main.class.getResource("layout/main.fxml"));
@@ -76,6 +115,10 @@ public class Main extends Application{
         primaryStage.show();
     }
 
+    /**
+     * Return the app's instance of the garbage collector
+     * @return Garbage Collector
+     */
     public GarbageCollection getGC() {
         return gc;
     }
